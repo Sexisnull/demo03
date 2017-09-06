6ac8ffd94844b286a414f861256c15c106f50db7
15810262545 <hothe@qq.com>
2017-09-06 15:26:17 +0800

    修改接口引擎设置及数据库配置

---
+++ Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/dao/sso/SsoDao.java
@@ -0,0 +1,24 @@
+package com.gsww.uids.gateway.dao.sso;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.gsww.uids.gateway.util.SpringContextHolder;
+
+public class SsoDao {
+	protected Logger logger = Logger.getLogger(getClass());
+	
+	private JdbcTemplate jdbcTemplate = SpringContextHolder.getBean("jdbcTemplate");
+	
+	public String findUrlByAppmark(String appmark){
+		
+		StringBuffer sql = new StringBuffer();
+		sql.append("select ssourl FROM jis_application where mark = ? ");
+		
+		Map<String,Object> map = jdbcTemplate.queryForMap(sql.toString(), appmark);
+		return String.valueOf(map.get("mark"));
+	}
+	
+}
--- Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/rest/AuthenticateService.java
+++ Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/rest/AuthenticateService.java
@@ -1,5 +1,48 @@
 package com.gsww.uids.gateway.rest;
 
-public class AuthenticateService {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gsww.uids.gateway.dao.sso.SsoDao;
+import com.gsww.uids.gateway.util.SpringContextHolder;
+
+@Path("/jis/sso")
+public class AuthenticateService {
+	
+	private SsoDao dao = SpringContextHolder.getBean("ssoDao");
+	
+	@GET
+	@Path("/usp.do")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public String usp(@QueryParam("action")String action,
+			@QueryParam("appmark")String appmark,
+			@QueryParam("redirectUrl")String redirectUrl,
+			@QueryParam("ticket")String ticket){
+		
+		if("ticketLogin".equals(action)){//统一登录
+			String url = dao.findUrlByAppmark(appmark);
+			return url;
+		}else if("register".equals(action)){//统一注册
+			
+		}else if("logout".equals(action)){//统一注销
+			
+		}else if("editPassword".equals(action)){//修改密码
+			
+		}else if("editUser".equals(action)){//修改个人信息
+			
+		}else{
+			
+		}
+		
+		
+		return "";
+	}
+	
 }
