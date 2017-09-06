93de7a992ff2c290f1d55731d135e7a65c077760
15810262545 <hothe@qq.com>
2017-09-06 18:02:03 +0800

    部分更新

--- Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/rest/AuthenticateService.java
+++ Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/rest/AuthenticateService.java
@@ -7,10 +7,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.gsww.uids.gateway.dao.sso.SsoDao;
 import com.gsww.uids.gateway.util.SpringContextHolder;
+import com.hanweb.sso.ldap.util.MD5;
+import com.sun.xml.bind.v2.TODO;
 
 @Path("/jis/sso")
 public class AuthenticateService {
@@ -26,23 +29,34 @@ public class AuthenticateService {
 			@QueryParam("redirectUrl")String redirectUrl,
 			@QueryParam("ticket")String ticket){
 		
-		if("ticketLogin".equals(action)){//统一登录
-			String url = dao.findUrlByAppmark(appmark);
-			return url;
-		}else if("register".equals(action)){//统一注册
-			
-		}else if("logout".equals(action)){//统一注销
-			
-		}else if("editPassword".equals(action)){//修改密码
-			
-		}else if("editUser".equals(action)){//修改个人信息
-			
+		if(StringUtils.isBlank(action)){
+			return "login.jsp";
 		}else{
+			if("ticketLogin".equals(action) && !StringUtils.isBlank(appmark)){//统一登录
+				String url = dao.findUrlByAppmark(appmark);
+				return url;
+			}else if(StringUtils.isBlank(appmark)){
+				return "没有传入应用标识！";
+			}
 			
+			if("register".equals(action)){//统一注册
+				return "login.jsp"; //TODO
+			}
+			if("logout".equals(action)){//统一注销
+				return "logout.jsp";//TODO
+			}
+			if("editPassword".equals(action) && !StringUtils.isBlank(ticket)){//修改密码
+				return "login.jsp"; //TODO
+			}else if("editPassword".equals(action) && StringUtils.isBlank(ticket)){
+				return "票据不能为空！";
+			}
+			if("editUser".equals(action)){//修改个人信息
+				return "login.jsp"; //TODO
+			}else if("editUser".equals(action) && StringUtils.isBlank(ticket)){
+				return "票据不能为空！";
+			}
 		}
-		
-		
-		return "";
+		return "系统错误！";
 	}
 	
 }
