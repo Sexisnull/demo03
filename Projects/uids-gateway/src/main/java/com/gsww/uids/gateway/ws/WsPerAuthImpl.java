f9eb484091c114ae72a28cb4bf5ccb2a29618d02
Administrator <957051968@qq.com>
2017-09-08 16:44:46 +0800

    个人/法人认证接口更新_1

--- Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/ws/WsPerAuthImpl.java
+++ Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/ws/WsPerAuthImpl.java
@@ -5,8 +5,10 @@ import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import com.gsww.sag.util.MD5;
+import com.chinatelecom.udb.util.TimeHelper;
+import com.gsww.uids.gateway.dao.perAuth.PerAuthDao;
 
 /**
  * Created on 2017-09-07
@@ -28,40 +30,79 @@ import com.gsww.sag.util.MD5;
 // 本地接口地址：http://127.0.0.1:8081/services/WsPerAuth?wsdl
 @WebService(serviceName = "WsPerAuth", endpointInterface = "com.gsww.uids.gateway.ws.WsPerAuth", portName = "WsPerAuthPort", targetNamespace = "http://www.gszwfw.gov.cn/gsjis/services/WsPerAuth")
 public class WsPerAuthImpl implements WsPerAuth {
+	PerAuthDao perAuthDao = new PerAuthDao();
 	protected Logger logger = Logger.getLogger(getClass());
 
 	@Override
 	// 个人-票据认证
 	public String ticketValidate(@WebParam(name = "appmark") String appmark, @WebParam(name = "ticket") String ticket,
 			@WebParam(name = "time") String time, @WebParam(name = "sign") String sign) {
-		/**
-		 * 字段 ---- 中文 ---- 数据库 appmark----应用标识----jis_application ticket ---- 票据
-		 * ---- time ---- ---- sign ---- ----
-		 * 
-		 */
-		logger.info("WsPerAuth-ticketValidate：" + "appmark=" + appmark + "ticket=" + ticket + "time=" + time + "sign="
-				+ sign);
+
 		if (appmark != null && ticket != null && time != null && sign != null) {
+			logger.info("WsPerAuth-ticketValidate：" + "appmark=" + appmark + "ticket=" + ticket + "time=" + time
+					+ "sign=" + sign);
+			System.out.println("WsPerAuth-ticketValidate：" + "appmark=" + appmark + "ticket=" + ticket + "time=" + time
+					+ "sign=" + sign);
+			/* 应用标识处理 */
+
+			/* 票据处理 */
+
+			/* 时间截处理 */
+			TimeHelper timeHelper = new TimeHelper();
+			time = timeHelper.getCurrentTime1();
+			/* 获取appword */
+
+			/* sign拼接 */
 
 			/* MD5加密 */
-			MD5 md5 = new MD5();
+			com.hanweb.sso.ldap.util.MD5 md5 = new com.hanweb.sso.ldap.util.MD5();
+			sign = md5.encrypt(appmark + "appword" + time, time);
+			/* 调用Dao查询票据信息，返回map.get("token") */
+
+			perAuthDao.findTokenByTicket(ticket);
+			/* 处理数据拼接成String类型的Json数据 */
+			String str = "";
+
+			return str;
 
 		} else {
 			return new String("{" + "errormsg：错误信息" + "}");
 		}
-		System.out.println("WsPerAuth-ticketValidate：" + "appmark=" + appmark + "ticket=" + ticket + "time=" + time
-				+ "sign=" + sign);
-		return "hello";
+
 	}
 
 	@Override
 	// 个人-根据令牌获取用户详细信息
 	public String findUserByToken(@WebParam(name = "appmark") String appmark, @WebParam(name = "token") String token,
 			@WebParam(name = "time") String time, @WebParam(name = "sign") String sign) {
-		logger.info("WsPerAuth-findUserByToken：" + "appmark=" + appmark + "token=" + token + "time=" + time + "sign="
-				+ sign);
-		System.out.println("WsPerAuth-findUserByToken：" + "appmark=" + appmark + "token=" + token + "time=" + time
-				+ "sign=" + sign);
+		if (appmark != null && token != null && time != null && sign != null) {
+			logger.info("WsPerAuth-findUserByToken：" + "appmark=" + appmark + "token=" + token + "time=" + time
+					+ "sign=" + sign);
+			System.out.println("WsPerAuth-findUserByToken：" + "appmark=" + appmark + "token=" + token + "time=" + time
+					+ "sign=" + sign);
+			/* appmark处理 */
+
+			/* token获取 */
+
+			/* 时间截处理 */
+			TimeHelper timeHelper = new TimeHelper();
+			time = timeHelper.getCurrentTime1();
+			/* 获取appword */
+			String appword = "";
+			/* sign拼接 */
+
+			/* MD5加密 */
+			com.hanweb.sso.ldap.util.MD5 md5 = new com.hanweb.sso.ldap.util.MD5();
+			sign = md5.encrypt(appmark + "appword" + time, time);
+
+			/* 调用Dao查询票据信息，返回map.get("token") */
+
+			perAuthDao.findUserByToken(token);
+			/* 处理数据拼接成String类型的Json数据 */
+			String str = "";
+
+			return str;
+		}
 		return null;
 	}
 
@@ -81,12 +122,41 @@ public class WsPerAuthImpl implements WsPerAuth {
 	// 个人-用户认证
 	public String userValidate(@WebParam(name = "appmark") String appmark, @WebParam(name = "sign") String sign,
 			@WebParam(name = "loginname") String loginname, @WebParam(name = "password") String password) {
-		logger.info("WsPerAuth-userValidate：" + "appmark=" + appmark + "sign=" + sign + "loginname=" + loginname
-				+ "password=" + password);
-		System.out.println("WsPerAuth-userValidate：" + "appmark=" + appmark + "sign=" + sign + "loginname=" + loginname
-				+ "password=" + password);
+		if (appmark != null && sign != null && loginname != null && password != null) {
+			logger.info("WsPerAuth-userValidate：" + "appmark=" + appmark + "sign=" + sign + "loginname=" + loginname
+					+ "password=" + password);
+			System.out.println("WsPerAuth-userValidate：" + "appmark=" + appmark + "sign=" + sign + "loginname="
+					+ loginname + "password=" + password);
+			/* appmark处理 */
+
+			/* 时间截处理 */
+			TimeHelper timeHelper = new TimeHelper();
+			String time = timeHelper.getCurrentTime1();
+			/* 获取appword */
+			String appword = "";
+			/* sign拼接 */
+
+			/* MD5加密 */
+			com.hanweb.sso.ldap.util.MD5 md5 = new com.hanweb.sso.ldap.util.MD5();
+			sign = md5.encrypt(appmark + "appword" + time, time);
+			/* loginname处理 */
+
+			/* password处理 */
+
+			/* 调用Dao查询票据信息，返回map.get("token") */
+			perAuthDao.findUserByLoginNameAndPassWord(loginname, password);
+
+			/* 处理数据拼接成String类型的Json数据 */
+			String str = "";
+
+			return str;
+		}
 		return null;
 	}
+
+	// --------------------------------------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------------------------------
+	// --------------------------------------------------------------------------------------------------------------------
 
 	// 个人-用户信息新增、修改
 	@WebMethod(operationName = "userSynResponse")
