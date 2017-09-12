9fc592e1daf9da129211a710766efac3490bc1e0
zhanghl <zhlhl_rjgc@163.com>
2017-09-12 16:18:10 +0800

    保存同步历史数据定时任务提交

--- Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/job/RecordSyncHisJob.java
+++ Projects/uids-gateway/src/main/java/com/gsww/uids/gateway/job/RecordSyncHisJob.java
@@ -1,33 +1,27 @@
 package com.gsww.uids.gateway.job;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
 import org.mule.api.MuleEventContext;
 import org.mule.api.lifecycle.Callable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.gsww.uids.gateway.dao.application.ApplicationDao;
 import com.gsww.uids.gateway.dao.sysview.SysViewDao;
-import com.gsww.uids.gateway.httpClient.MyHttpClient;
 import com.gsww.uids.gateway.util.SpringContextHolder;
 
-/**
- * Description：实时同步数据 JOB
- * @author zhl
- * @version v2.0
- * */
-public class ImmediateSyncJob implements Callable{
+public class RecordSyncHisJob implements Callable{
+
 	protected Logger logger = LoggerFactory.getLogger(getClass());
 	
-	//实时同步数据任务队列
+	//当前同步数据任务队列
 	private ArrayBlockingQueue<Runnable> taskQueue = new ArrayBlockingQueue<Runnable>(20);
-	
-	private SysViewDao sysViewDao = SpringContextHolder.getBean("sysViewDao");
+		
+    private SysViewDao sysViewDao = SpringContextHolder.getBean("sysViewDao");
 	
 	@Override
 	public Object onCall(MuleEventContext eventContext) throws Exception {
@@ -37,19 +31,19 @@ public class ImmediateSyncJob implements Callable{
 	@SuppressWarnings("static-access")
 	public void bizDeal(){
 		try {
-			logger.info("定时同步数据程序启动");
+			logger.info("定时保存历史同步数据程序启动");
 			
-			List<Map<String, Object>> syncMapList = null; 
+			List<Map<String, Object>> currMapList = null; //当前同步列表
 			ThreadPoolExecutor threadPool = new ThreadPoolExecutor(20, 30, 3, TimeUnit.SECONDS, taskQueue,new ThreadPoolExecutor.CallerRunsPolicy());
 			
 			while(true){
-				//检查实时同步数据队列是否为空，若为空则查询并执行任务
+				//检查实时同步列表队列是否为空，若为空则查询并执行任务
 				if(taskQueue.isEmpty()){
-					syncMapList = sysViewDao.findSysView();
-					if(syncMapList!=null && !syncMapList.isEmpty()) {
-						logger.info("当前共读取实时同步数据:" + syncMapList.size() + "条记录！");
-						for (Map<String, Object> syncMap : syncMapList) {
-							threadPool.execute(new ImmediateSyncThread(syncMap));
+					currMapList = sysViewDao.findCurrSysView();
+					if(currMapList!=null && !currMapList.isEmpty()) {
+						logger.info("共读取当前同步数据:" + currMapList.size() + "条记录！");
+						for (Map<String, Object> currMap : currMapList) {
+							threadPool.execute(new RecordSyncHisTread(currMap));
 						}
 					}
 				}
@@ -61,4 +55,5 @@ public class ImmediateSyncJob implements Callable{
 			bizDeal();
 		}
 	}
+	
 }
