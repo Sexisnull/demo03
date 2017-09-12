1bf1b12b6ffc56a04ce599e23ad7eb5b66b8e18e
15393112460 <383117348@qq.com>
2017-09-12 09:11:32 +0800

    change groupservice method

--- Projects/uids-web/src/main/java/com/gsww/uids/service/ComplatGroupService.java
+++ Projects/uids-web/src/main/java/com/gsww/uids/service/ComplatGroupService.java
@@ -2,10 +2,12 @@ package com.gsww.uids.service;
 
 import java.util.List;
 
+import org.springframework.data.jpa.domain.Specification;
+
 import com.gsww.uids.entity.ComplatGroup;
 
 public interface ComplatGroupService {
 	
-	public List<ComplatGroup> findAll();
+	public List<ComplatGroup> findAll(Specification<ComplatGroup> spec);
 	
 }
