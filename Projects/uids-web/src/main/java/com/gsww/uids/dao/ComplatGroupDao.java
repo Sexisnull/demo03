087a68aea2d5f452b939a8299ef1be5c7a0a22ac
xyy <xyy-JAVA@outlook.com>
2017-10-18 10:56:48 +0800

    update groupDao

--- Projects/uids-web/src/main/java/com/gsww/uids/dao/ComplatGroupDao.java
+++ Projects/uids-web/src/main/java/com/gsww/uids/dao/ComplatGroupDao.java
@@ -57,7 +57,9 @@ JpaSpecificationExecutor<ComplatGroup>{
 	 */
 	@Query(value = "(SELECT * FROM complat_group where iid =?1) " +
 			" UNION (SELECT * FROM complat_group where pid =?1)" +
-			" UNION (SELECT * FROM complat_group where pid in ((SELECT iid FROM complat_group where pid =?1)))",nativeQuery=true)
+			" UNION (SELECT * FROM complat_group where pid in (SELECT iid FROM complat_group where pid =?1))" +
+			" UNION (SELECT * FROM complat_group where pid in (SELECT cg.iid FROM complat_group cg where pid in (SELECT iid FROM complat_group where pid =?1)))"
+			,nativeQuery=true)
 	public List<ComplatGroup> findAllDepId(String detId);
 //	@Query(value = "select group from ComplatGroup group where group.pid=? order by group.orderid asc")
 //	public List<ComplatGroup> findByNoPid();
