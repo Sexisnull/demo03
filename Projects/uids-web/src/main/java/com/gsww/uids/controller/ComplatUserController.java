7fa4831010a4e584cfe8ea06ea64af06926ec49e
yaoxi <526011695@qq.com>
2017-10-20 11:34:55 +0800

    用户设置增加日志

--- Projects/uids-web/src/main/java/com/gsww/uids/controller/ComplatUserController.java
+++ Projects/uids-web/src/main/java/com/gsww/uids/controller/ComplatUserController.java
@@ -987,6 +987,10 @@ public class ComplatUserController extends BaseController {
 						jisUserdetailService.update(jisUserdetail.getIid(),
 								cardId, userMap);
 					}
+					
+					//日志
+					String desc = complatUser.getLoginname()+ "修改用户设置";
+					jisLogService.save(complatUser.getLoginname(),this.getIpAddr(request), desc, 2, 2);
 
 					// 同步
 					List<Map<String, Object>> syListMap = complatUserService
@@ -1089,6 +1093,23 @@ public class ComplatUserController extends BaseController {
 		}
 
 	}
+	
+	/**
+	 * 获取客户端IP
+	 */
+	  private String getIpAddr(HttpServletRequest request) {     
+	      String ip = request.getHeader("x-forwarded-for");     
+	      if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	         ip = request.getHeader("Proxy-Client-IP");     
+	     }     
+	      if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	         ip = request.getHeader("WL-Proxy-Client-IP");     
+	      }     
+	     if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {     
+	          ip = request.getRemoteAddr();     
+	     }     
+	     return ip;     
+	}  
 	/**
 	 * 获取用户扩展属性
 	 * 
