0f4b5212b6c7fae8234ae4947bf4a71c2aa85d0b
yaoxi <526011695@qq.com>
2017-10-24 11:23:51 +0800

    修改用户扩展属性保存身份证号

--- Projects/uids-web/src/main/java/com/gsww/uids/service/impl/JisUserdetailServiceImpl.java
+++ Projects/uids-web/src/main/java/com/gsww/uids/service/impl/JisUserdetailServiceImpl.java
@@ -56,13 +56,13 @@ public class JisUserdetailServiceImpl implements JisUserdetailService {
 		}
 		String updateSql = "";
 		if(StringHelper.isNotBlack(cardId) && StringHelper.isNotBlack(whereSqls)){
-			updateSql = "update jis_userdetail t set t.cardid = "+cardId+","+whereSqls+" where t.iid = "+iid;
+			updateSql = "update jis_userdetail t set t.cardid = '"+cardId+"',"+whereSqls+" where t.iid = "+iid;
 			jdbcDAO.execute(updateSql);
 		}else if(!StringHelper.isNotBlack(cardId) && StringHelper.isNotBlack(whereSqls)){
 			updateSql = "update jis_userdetail t set "+whereSqls+" where t.iid = "+iid;
 			jdbcDAO.execute(updateSql);
 		}else if(StringHelper.isNotBlack(cardId) && !StringHelper.isNotBlack(whereSqls)){
-			updateSql = "update jis_userdetail t set t.cardid = "+cardId+" where t.iid = "+iid;
+			updateSql = "update jis_userdetail t set t.cardid = '"+cardId+"' where t.iid = "+iid;
 			jdbcDAO.execute(updateSql);
 		}		
 	}
